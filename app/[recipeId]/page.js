fb065ae01d3fab5c4a8eba7a539113d4f4a47d82
Haris Raees <haris.pixako1@gmail.com>
2025-10-28 16:13:43 +0500

    Add loading skeleton, create error component, added images through remotePatterns

--- app/[recipeId]/page.js
+++ app/[recipeId]/page.js
@@ -1,28 +1,39 @@
-export async function generateMetaData({params}) {
-    const { recipeId } = params;
-    return{
-        title: `${recipeId} title`,
-        description: `${recipeId} desc goes here`,
-    };
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+  const { recipeId } = await params;
+
+  return {
+    title: `${recipeId} title`,
+    description: `${recipeId} description goes here`,
+  };
 }
 
 export async function generateStaticParams() {
-    const recipes = await fetch("https://dummyjson.com/recipes").then((res) => {
-        res.json();
-    });
+  const recipes = await fetch("https://dummyjson.com/recipes").then((res) =>
+    res.json(),
+  );
 
-    return recipes.recipes.map((recipe) => ({
-        recipeId: recipe.id.toString(),
-    }));
+  return recipes.recipes.map((recipe) => ({
+    recipeId: recipe.id.toString(),
+  }));
 }
 
 export default async function Recipe({ params }) {
-    const {recipeId} = await params;
+  const { recipeId } = await params;
 
-    const res = await fetch(`https://dummyjson.com/recipes/${recipeId}`);
-    const recipe = await res.json();
+  // throw new Error("Something went wrong");
 
-    return (
-        <h1 className="text-center font-bold p-2">{recipe.name}</h1>
-    );
+  const res = await fetch(`https://dummyjson.com/recipes/${recipeId}`);
+  const recipe = await res.json();
+
+  if (!recipe.id) notFound();
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Image alt={recipe.name} src={recipe.image} width={200} height={200} />
+      <h1 className="text-center font-bold p-5">{recipe.name}</h1>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,20 +1,40 @@
 import Link from "next/link";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+// Dynamically import the HeavyComponent component with suspense support
+const HeavyComponent = dynamic(() => import("./components/heavy-component"), {
+  suspense: true,
+});
 
 export default async function Home() {
   const res = await fetch("https://dummyjson.com/recipes", {
     next: {
-      revalidate: 60, //seconds
-    }
+      revalidate: 60, // seconds
+    },
   });
   const recipes = await res.json();
 
   return (
-    <ol className="nl-auto text-center">
-      {recipes.recipes.map((recipe) => (
-        <li key={recipe.id}>
-          <Link href={`/${recipe.id}`}>{recipe.name}</Link>
-        </li>
-      ))}
-    </ol>
-  )
+    <>
+      {/* Suspense boundary with a fallback */}
+      {/* <Suspense
+        fallback={
+          <h1 className="text-center font-bold pb-20">
+            Loading user profile...
+          </h1>
+        }
+      >
+        <HeavyComponent />
+      </Suspense> */}
+
+      <ol className="ml-auto text-center">
+        {recipes.recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link href={`/${recipe.id}`}>{recipe.name}</Link>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
