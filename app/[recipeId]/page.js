d63b14d4be9f6624ebac5e6a69fd20ef88b40360
Haris Raees <haris.pixako1@gmail.com>
2025-10-28 12:34:24 +0500

    create metaData func for Dynamic routes

--- app/[recipeId]/page.js
+++ app/[recipeId]/page.js
@@ -1,7 +1,18 @@
+export async function generateMetaData({params}) {
+   
+    const { recipeId } = params;
+
+    return{
+        title: `${recipeId} title`,
+        description: `${recipeId} desc goes here`,
+    };
+}
+
 export default async function Recipe({ params }) {
     const {recipeId} = await params;
 
     return (
         <h1 className="text-center font-bold p-2">{recipeId}</h1>
+        
     );
 }
