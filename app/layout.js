d7823779d1239c709ad6d044d8c272d057a96456
Haris Raees <haris.pixako1@gmail.com>
2025-10-28 12:25:04 +0500

    Added Navbar for navigation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className="text-sm flex justify-center space-x-8 p-20">
+          <Link href="/">Home</Link>
+          <Link href="/add">Add</Link>
+        </nav>
         {children}
       </body>
     </html>
