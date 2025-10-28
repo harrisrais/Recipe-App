3b0823fa386aa9fcabcbffbdad69f351d53ee33d
Haris Raees <haris.pixako1@gmail.com>
2025-10-28 14:21:28 +0500

    create getStaticParams for static recipes in dymaic page

--- app/[recipeId]/page.js
+++ app/[recipeId]/page.js
@@ -6,10 +6,20 @@ export async function generateMetaData({params}) {
     };
 }
 
+export async function generateStaticParams() {
+    const recipes = await fetch("https://dummyjson.com/recipes").then((res) => {
+        res.json();
+    });
+
+    return recipes.recipes.map((recipe) => ({
+        recipeId: recipe.id.toString(),
+    }));
+}
+
 export default async function Recipe({ params }) {
     const {recipeId} = await params;
 
-    const res = await fetch("https://dummyjson.com/recipes/${recipeId}");
+    const res = await fetch(`https://dummyjson.com/recipes/${recipeId}`);
     const recipe = await res.json();
 
     return (
